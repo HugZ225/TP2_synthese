7f6cbb46e3d8666b6c1469b56f6fda2c2b1915a4
HugZ225 <162215285+HugZ225@users.noreply.github.com>
2025-01-10 15:33:45 +0100

    Update TP2_synthese.c

--- TP2_synthese.c
+++ TP2_synthese.c
@@ -6,17 +6,18 @@
 
 int main(int argc, char ** argv) {
     printf("TFTP Client \n");
-
+    //if correct number of arguments is provided (host, port, file)
     if (argc != 4){
          printf("Wrong command \n");
          exit(EXIT_FAILURE);
      }
-
+   
     struct addrinfo hints;
     struct addrinfo * result;
+    // Clear the hints structure to avoid undefined behavior
     memset(&hints,0,sizeof(struct addrinfo));
 
-    hints.ai_family = AF_INET;
+    hints.ai_family = AF_INET; //IPv4 addresses
     hints.ai_protocol = IPPROTO_UDP;
 
     int status = getaddrinfo(argv[1],argv[2],&hints,&result);
@@ -29,15 +30,17 @@ int main(int argc, char ** argv) {
              NI_NUMERICHOST|NI_NUMERICSERV);
 
     printf("%s is resolved at : %s:%s",argv[1],ip_string,port_string);
-
+    // Create a socket using the resolved address family, type, and protocol
     int sd = socket(result->ai_family,result->ai_socktype,
          result->ai_protocol);
 
+    // TFTP Read Request (RRQ) message
     char rrq[128]={0};
     rrq[1]=1;
     sprintf(rrq+2,"%s",argv[3]);
     sprintf(rrq+3+strlen(argv[3]),"octet");
 
+    // Send the RRQ message to the TFTP server
     sendto(sd,rrq,strlen(argv[3])+9,NULL,
         result->ai_addr,result->ai_addrlen);
 
@@ -45,12 +48,12 @@ int main(int argc, char ** argv) {
     struct sockaddr server_addr;
     socklen_t addr_len = sizeof(server_addr);
 
+    // Receive data from the server (response or data packet)
     ssize_t n_bytes = recvfrom(sd, buffer, BUF_SIZE , 0, (struct sockaddr *)&server_addr, &addr_len)
     if (n_bytes<0){
         perror("Error data");
         exit(EXIT_FAILURE);}
-
-
+    // Free the memory allocated for address resolution
     freeaddrinfo(result);
 
 
